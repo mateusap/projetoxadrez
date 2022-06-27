38e5d1dcb5f40a5063475a8801d5b550f3591e92
Mateus Silva <ma.aparecido.silva@hotmail.com>
2022-06-27 09:41:39 -0300

    Inicio da implementação de peça e tabuleiro

---
+++ Xadrezconsole/tabuleiroetc/Peca.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiroetc
+{
+    internal class Peca
+    {
+        public Posicao posicao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtmovimento { get; protected set; }
+        public Tabuleiro tab { get; protected set; }
+
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        {
+            this.posicao = posicao;
+            this.cor = cor;
+            this.qtmovimento = qtmovimento = 0;
+            this.tab = tab;
+        }
+    }
+}
