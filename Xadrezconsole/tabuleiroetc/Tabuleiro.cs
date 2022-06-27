1c36ac7c40ccc06b690995b763bb3d0cc72973cd
Mateus Silva <ma.aparecido.silva@hotmail.com>
2022-06-27 10:46:07 -0300

    Criação de exceção

--- Xadrezconsole/tabuleiroetc/Tabuleiro.cs
+++ Xadrezconsole/tabuleiroetc/Tabuleiro.cs
@@ -22,10 +22,38 @@ namespace tabuleiroetc
         {
             return pecas[linha, coluna];
         }
+        public bool casaOcupada (Posicao pos)
+        {
+            validar(pos);
+            return peca(pos) != null;
+        }
+        private object peca(Posicao pos)
+        {
+            throw new NotImplementedException();
+        }
         public void posicionar (Peca p, Posicao pos)
         {
+            if (casaOcupada(pos))
+            {
+                throw new TabuleiroException("Essa casa já está ocupada");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
+        }
+        public bool posValida (Posicao pos)
+        {
+            if (pos.linha<0 || pos.coluna<0 || pos.linha >= linha || pos.coluna >= coluna)
+            {
+                return false;
+            }
+            return true;
+        }
+        public void validar (Posicao pos)
+        {
+            if (!posValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
         }
     }
 }
